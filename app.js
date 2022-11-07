619596914d7946b9e2116c0db42e25f1aaf8a31c
maddenlockin <maddenlockin@gmail.com>
2022-11-07 11:13:45 -0800

    form creating new chick based on user input

--- app.js
+++ app.js
@@ -13,8 +13,29 @@ const chicks = [
     { id: 2, name: 'Shellie', hp: 3 },
     { id: 3, name: 'Tabitha', hp: 2 },
 ];
+let currentId = 4;
 
 /* Events */
+formEl.addEventListener('submit', (e) => {
+    // prevent default behavior of a form submit
+    e.preventDefault();
+
+    const data = new FormData(formEl);
+
+    //make a new chick object with the user input
+    const newChick = {
+        id: currentId,
+        name: data.get('chick-name'),
+        hp: Math.ceil(Math.random() * 5),
+    };
+
+    currentId++;
+
+    //add newChick object to the array of chicks in state
+    chicks.push(newChick);
+
+    displayChicks();
+});
 
 /* Display Functions */
 function displayChicks() {
